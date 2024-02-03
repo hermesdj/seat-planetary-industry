e4250a1a9fc03a8258fa253236bd9d916c6b5c13
Jay's <dallardj@gmail.com>
2024-02-03 09:59:54 +0100

    fix tier infos seeder

--- src/database/seeds/TierInfoSeeder.php
+++ src/database/seeds/TierInfoSeeder.php
@@ -11,31 +11,26 @@ class TierInfoSeeder extends Seeder
         [
             'tier_id' => 0,
             'market_group_id' => 1333,
-            'quantity_consumed' => 0,
             'quantity_produced' => 0
         ],
         [
             'tier_id' => 1,
             'market_group_id' => 1334,
-            'quantity_consumed' => 3000,
             'quantity_produced' => 20
         ],
         [
             'tier_id' => 2,
             'market_group_id' => 1335,
-            'quantity_consumed' => 40,
             'quantity_produced' => 5
         ],
         [
             'tier_id' => 3,
             'market_group_id' => 1336,
-            'quantity_consumed' => 10,
             'quantity_produced' => 6
         ],
         [
             'tier_id' => 4,
             'market_group_id' => 1337,
-            'quantity_consumed' => 6,
             'quantity_produced' => 1
         ]
     ];
