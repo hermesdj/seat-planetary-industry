c8eebfa428b462d8b0cef66e23c327e190d5911b
Jay's <dallardj@gmail.com>
2025-01-01 19:28:54 +0100

    Update 2024_12_27_191800_remove_foreign_index.php

--- src/database/migrations/2024_12_27_191800_remove_foreign_index.php
+++ src/database/migrations/2024_12_27_191800_remove_foreign_index.php
@@ -6,12 +6,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+    function hasForeignKey(string $table, string $column): bool
+    {
+        $fkColumns = Schema::getConnection()->getDoctrineSchemaManager()->listTableForeignKeys($table);
+        return collect($fkColumns)->map(function ($fkColumn) {
+            return $fkColumn->getColumns();
+        })->flatten()->contains($column);
+    }
+
     public function up(): void
     {
+        Schema::disableForeignKeyConstraints();
         Schema::table('pi_tier_infos', function (Blueprint $table) {
-            $table->dropForeign('pi_tier_infos_market_group_id_foreign');
+            if ($this->hasForeignKey('pi_tier_infos', 'market_group_id')) {
+                $table->dropForeign('pi_tier_infos_market_group_id_foreign');
+            }
         });
+        Schema::enableForeignKeyConstraints();
     }
 };
