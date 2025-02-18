5e30be0ccc304ceb686935d539592235b0d19f93
Jay's <dallardj@gmail.com>
2025-02-18 14:36:43 +0100

    account pi menu order
    putting the storage before the projects makes sense

--- src/resources/views/account/storage.blade.php
+++ src/resources/views/account/storage.blade.php
@@ -4,22 +4,22 @@
 
 @push('javascript')
     <script>
-        $(document).ready(function() {
-            var table = $('#storageTable').DataTable();
+        $(document).ready(function () {
+            let table = $('#storageTable').DataTable();
 
             function filterTable() {
-                var showStorageFacility = $('#storageFacility').is(':checked');
-                var showLaunchPad = $('#launchPad').is(':checked');
+                let showStorageFacility = $('#storageFacility').is(':checked');
+                let showLaunchPad = $('#launchPad').is(':checked');
 
-                var storageFacilityIds = [2541, 2536, 2257, 2558, 2535, 2560, 2561, 2562];
-                var launchPadIds = [2544, 2543, 2552, 2555, 2542, 2556, 2557, 2256];
+                let storageFacilityIds = [2541, 2536, 2257, 2558, 2535, 2560, 2561, 2562];
+                let launchPadIds = [2544, 2543, 2552, 2555, 2542, 2556, 2557, 2256];
 
                 $.fn.dataTable.ext.search = [];
-                $.fn.dataTable.ext.search.push(function(settings, data, dataIndex) {
-                    var storageType = parseInt($(table.row(dataIndex).node()).data('storage-type'));
+                $.fn.dataTable.ext.search.push(function (settings, data, dataIndex) {
+                    let storageType = parseInt($(table.row(dataIndex).node()).data('storage-type'));
                     if ((storageFacilityIds.includes(storageType) && showStorageFacility) ||
-                    (launchPadIds.includes(storageType) && showLaunchPad)) {
-                    return true;
+                        (launchPadIds.includes(storageType) && showLaunchPad)) {
+                        return true;
                     }
                     return false;
                 });
